0b695ccc6b5876a8231b65085ca8a3994ed66629
paul casey <paulcaseys@gmail.com>
2013-07-29 21:09:32 +1000

    initial commit

---
+++ prototypes/widget-masters-01/Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,55 @@
+﻿for(var i = 0; i < 13; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u1'] = 'center';document.getElementById('u2_img').tabIndex = 0;
+HookHover('u2', false);
+
+u2.style.cursor = 'pointer';
+$axure.eventManager.click('u2', function(e) {
+
+if (true) {
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u3'] = 'center';document.getElementById('u4_img').tabIndex = 0;
+HookHover('u4', false);
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Page_1.html');
+
+}
+});
+gv_vAlignTable['u5'] = 'center';document.getElementById('u6_img').tabIndex = 0;
+HookHover('u6', false);
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Page_2.html');
+
+}
+});
+gv_vAlignTable['u7'] = 'center';document.getElementById('u8_img').tabIndex = 0;
+HookHover('u8', false);
+
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Page_3.html');
+
+}
+});
+gv_vAlignTable['u9'] = 'center';
---
+++ prototypes/widget-masters-01/Page_2_files/axurerp_pagespecificscript.js
@@ -0,0 +1,66 @@
+﻿for(var i = 0; i < 16; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+u15.tabIndex = 0;
+
+u15.style.cursor = 'pointer';
+$axure.eventManager.click('u15', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Page_3.html');
+
+}
+});
+gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u1'] = 'center';document.getElementById('u2_img').tabIndex = 0;
+HookHover('u2', false);
+
+u2.style.cursor = 'pointer';
+$axure.eventManager.click('u2', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+gv_vAlignTable['u3'] = 'center';document.getElementById('u4_img').tabIndex = 0;
+HookHover('u4', false);
+
+u4.style.cursor = 'pointer';
+$axure.eventManager.click('u4', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Page_1.html');
+
+}
+});
+gv_vAlignTable['u5'] = 'center';document.getElementById('u6_img').tabIndex = 0;
+HookHover('u6', false);
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u7'] = 'center';document.getElementById('u8_img').tabIndex = 0;
+HookHover('u8', false);
+
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Page_3.html');
+
+}
+});
+gv_vAlignTable['u9'] = 'center';
